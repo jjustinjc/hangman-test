b9a7a089a5763853f0fb8ac0508d6d3001dc653d
jjustinjc <caijjustin@gmail.com>
2025-05-27 14:23:30 -0400

    letsgo

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import random
-
+import random, sys
 words = ["tiger", "tree", "underground", "giraffe", "chair"]
 
 remaining_attempts = 5
@@ -17,26 +16,44 @@ def print_hidden(hidden):
     print("\n")
 
 def check_letter(guess):
-    if len(guess) > 1 or not guess.isalpha() or guess in guessed_letters:
+    if len(guess) > 1 or not guess.isalpha():
         print("Invalid input!")
-        return False
+        return True
+    if guess in guessed_letters:
+        print("Invalid input!")
+        return True
     if guess not in secret_word:
         print("wrong")
         return False
     return True
 
+def check_win(word):
+    for c in word:
+        if c not in guessed_letters:
+            return False
+    return True
 
 secret_word = select_word(words)
 print(secret_word)
+
 while remaining_attempts > 0:
     print_hidden(secret_word)
     print(guessed_letters)
     print(remaining_attempts)
     guess = input("Type a letter: ")
     while not check_letter(guess):
-        guess = input("Type a letter: ")
         remaining_attempts -= 1
-
+        if remaining_attempts == 0:
+            break
+        print(remaining_attempts)
+        guess = input("Type a letter: ")
     guessed_letters += guess
+
+    if check_win(secret_word):
+        print("WIN!!")
+        sys.exit()
+
+print("LOSE!!")
+sys.exit()
 
 
